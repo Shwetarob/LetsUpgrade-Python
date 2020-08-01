c97affd25dc0677dbfb7baca618201ebd48210c1
Shwetarob <68479074+Shwetarob@users.noreply.github.com>
2020-08-01 21:37:30 +0530

    Add files via upload

---
+++ day4 shweta.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+#Day 4 assignment- shweta
+
+
+# In[17]:
+
+
+#question1
+
+
+# In[18]:
+
+
+#Find all the occurrence of substring in the given string
+
+
+# In[20]:
+
+
+
+givenstring="What we think we become; we are python programmer"
+for i in range(len(givenstring)):
+    if givenstring.startswith("we",i):
+        print("The start index of string are",i)
+
+
+# In[21]:
+
+
+#Isupper()
+
+
+# In[22]:
+
+
+str2="world"
+str2.isupper()
+
+
+# In[23]:
+
+
+str2="WORLD"
+str2.isupper()
+
+
+# In[24]:
+
+
+str2="World"
+str2.isupper()
+
+
+# In[25]:
+
+
+#IsLower()
+
+
+# In[26]:
+
+
+
+str3 = "helloworld"
+str3.islower()
+
+
+# In[27]:
+
+
+str3 = "Helloworld"
+str3.islower()
+
+
+# In[28]:
+
+
+str3 = "HELLOWORLD"
+str3.islower()
+
+
+# In[ ]:
+
+
+
+
