c1572c8f5dbb173ba3d60f9ce6ba252aeff72ab6
Shwetarob <68479074+Shwetarob@users.noreply.github.com>
2020-08-01 21:58:42 +0530

    Add files via upload

---
+++ day 6 shweta.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#day6 assignment- shweta 
+
+
+# In[2]:
+
+
+#question
+
+
+# In[4]:
+
+
+#List a and List b is given. Convert to Dictionary using list comprehension
+
+
+# In[6]:
+
+
+Lista = [1,2,3,4,5,7,8]
+Listb = ["a","b","c","d","e"]
+print(Lista,Listb)
+
+
+# In[7]:
+
+
+each={Lista[i]:Listb[i] for i in range(len(Listb))}
+print(each)
+
+
+# In[ ]:
+
+
+
+
